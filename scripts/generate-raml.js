950210e109245b570aacfc9bfc5ee8a12382f5d1
Kevin He <khe@mobify.me>
2017-10-18 11:03:45 -0700

    generate separate files

--- scripts/generate-raml.js
+++ scripts/generate-raml.js
@@ -6,28 +6,28 @@ const raml = fs.readFileSync(`src/${version}/main.raml`).toString()
 const html = fs.readFileSync(`src/${version}/main.html`, 'utf8') // beautify main.html before reading, single line too long
 const $ = cheerio.load(html, {decodeEntities: false})
 
-const outputLocation = `${version}-output.raml`
+// const outputLocation = `${version}-output.raml`
 
 const pattern = /(?:request|response)\.(\..*(GET|POST|PUT|PATCH|DELETE))\./
 
-const output = raml.split('\n')
+raml.split('\n')
     .filter((line) => pattern.test(line))
-    .reduce((accumulator, string) => {
-        const link = string.split(':')[1]
-        const method = string.match(/(?:GET|POST|PUT|PATCH|DELETE)/)[0]
+    .forEach((line) => {
+        const fileName = line.split('!include ')[1]
+        const method = line.match(/(?:GET|POST|PUT|PATCH|DELETE)/)[0]
 
-        const id = string.match(pattern)[1]
+        const id = line.match(pattern)[1]
             .replace(/\./g, '_')
             .replace(method, method.toLowerCase())
 
         const schema = $(`#${id}`).find('code').html()
-        return accumulator.replace(link, schema)
+
+        fs.writeFile(fileName, schema, 'utf8', (err) => {
+            if (err) {
+                throw new Error(err)
+            } else {
+                console.log(`Description file is generated: ${outputLocation}`)
+            }
+        })
     }, raml)
 
-fs.writeFile(outputLocation, output, 'utf8', (err) => {
-    if (err) {
-        return console.log(err);
-    } else {
-        console.log(`Complete Raml file for Hybris OCC is generated: ${outputLocation}`)
-    }
-})
