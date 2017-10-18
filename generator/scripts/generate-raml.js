6c2268b4012c40bd2b44444a070a61b2884a05f0
Kevin He <khe@mobify.me>
2017-10-18 13:17:42 -0700

    generator folder

---
+++ generator/scripts/generate-raml.js
@@ -0,0 +1,44 @@
+
+const version = process.argv[2] || require('../package.json').target
+const fs = require('fs')
+const path = require('path')
+const cheerio = require('cheerio')
+const raml = fs.readFileSync(`src/${version}/main.raml`).toString()
+const html = fs.readFileSync(`src/${version}/main.html`, 'utf8') // beautify main.html before reading, single line too long
+const $ = cheerio.load(html, {decodeEntities: false})
+
+// const outputLocation = `${version}-output.raml`
+
+const pattern = /(?:request|response)\.(\..*(GET|POST|PUT|PATCH|DELETE))\./
+
+raml.split('\n')
+    .filter((line) => pattern.test(line))
+    .forEach((line) => {
+        const fileName = line.split('!include ')[1]
+        const method = line.match(/(?:GET|POST|PUT|PATCH|DELETE)/)[0]
+
+        const id = line.match(pattern)[1]
+            .replace(/\./g, '_')
+            .replace(method, method.toLowerCase())
+
+        let schema = $(`#${id}`).find('code')
+            .html()
+            
+        const title = JSON.parse(schema).title
+
+        // Fix a bug from source file provided by Hybris
+        // if a property of the schema is refering to itself
+        // change ref to '#'
+        schema = schema.replace(`#/definitions/${title}`, '#')
+        
+        const location = path.join('dist/', fileName)
+
+        fs.writeFile(location, schema, 'utf8', (err) => {
+            if (err) {
+                throw new Error(err)
+            } else {
+                console.log(`Description file is generated: ${location}`)
+            }
+        })
+    }, raml)
+
