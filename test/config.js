cac5c383276fe31a983650605646df2259c9a88c
Kevin He <k.he933@gmail.com>
2017-11-01 21:50:52 -0700

    add copyrights

--- test/config.js
+++ test/config.js
@@ -1,3 +1,7 @@
+/* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
+/* Copyright (c) 2017 Mobify Research & Development Inc. All rights reserved. */
+/* * *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * */
+
 import * as OCC from '../src/index'
 
 const testWrapper = (occ) => {
